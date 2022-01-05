cd975e2f94427f41a0193f9879270686cd7474a5
Yeapes <yeapesh2831@gmail.com>
2022-01-05 23:52:55 +0600

    Collection Product in home added

--- src/components/home/Collection.js
+++ src/components/home/Collection.js
@@ -1,13 +1,101 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component,Fragment} from 'react';
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 class Collection extends Component {
-    render() { 
-        return(
+    render() {
+        return (
             <Fragment>
+                <Container  className="text-center bg-white card-body shadow-sm py-5 BetweenTwoSection" fluid={true}>
+                    <h4 className="section-title ">SPECIAL COLLECTION</h4>
+                    <h6 className="section-sub-title pb-3">Some Of Our Exclusive Collection, You May Like</h6>
+                    <Row >
+                        <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col  className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col  className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6} >
+                            <Card className="card text-center w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                    </Row>
+                </Container>
+
 
             </Fragment>
         );
     }
 }
- 
+
 export default Collection;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import Categories from '../components/home/Categories';
+import Collection from '../components/home/Collection';
 import FeaturedProducts from '../components/home/FeaturedProducts';
 class HomePage extends  Component {
     render() { 
         return(
             <Fragment>
                 <FeaturedProducts />
+                <Categories />
+                <Collection />
             </Fragment>
         );
     }
