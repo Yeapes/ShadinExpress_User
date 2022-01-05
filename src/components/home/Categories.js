4e3afdba54e724262f1f1305a247b8910e154a3b
Yeapes <yeapesh2831@gmail.com>
2022-01-05 23:28:24 +0600

    Category added

--- src/components/home/Categories.js
+++ src/components/home/Categories.js
@@ -1,12 +1,244 @@
-import React, { Component, Fragment } from 'react';
-class Categories extends React.Component {
-    render() { 
-        return(
+import React, {Component,Fragment} from 'react';
+import {Card, Col, Container, Row} from "react-bootstrap";
+
+class Categories extends Component {
+    render() {
+        return (
             <Fragment>
+                <Container className="text-center pt-3  BetweenTwoSection" fluid={true}>
+                    <h4 className="section-title">CATEGORIES</h4>
+                    <h6 className="section-sub-title pb-3">Some Of Our Exclusive Collection, You May Like</h6>
+                    <Row>
+                        <Col  key={1} xl={6} lg={6} md={6} sm={12} xs={12} >
+                            <Row>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+
+
+
+                        <Col  key={2} xl={6} lg={6} md={6} sm={12} xs={12} >
+                            <Row>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                        <Col  key={1} xl={6} lg={6} md={6} sm={12} xs={12} >
+                            <Row>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                        <Col  key={1} xl={6} lg={6} md={6} sm={12} xs={12} >
+                            <Row>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                        <Col  key={1} xl={6} lg={6} md={6} sm={12} xs={12} >
+                            <Row>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                        <Col  key={1} xl={6} lg={6} md={6} sm={12} xs={12} >
+                            <Row>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6} >
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="w-75" src="https://static-01.daraz.com.bd/original/3d5fbadc876cbec076be088f0b3b581e.jpg"/>
+                                            <h5 className="category-name">CATEGORY NAME</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                    </Row>
+                </Container>
 
             </Fragment>
         );
     }
 }
- 
+
 export default Categories;
