ce4367480aadcef6572fbfa1430164514040a717
Yeapes <yeapesh2831@gmail.com>
2022-01-06 22:37:01 +0600

    Newarrival product added

--- src/components/home/Newarraival.js
+++ src/components/home/Newarraival.js
@@ -1,12 +1,125 @@
-import React, { Component, Fragment } from 'react';
-class Newarraival extends Component {
-    render() { 
-        return(
-            <Fragment>
+import React, {Component,Fragment} from 'react';
+import Slider from "react-slick";
+import {Card, Col, Container, Row} from "react-bootstrap";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-            </Fragment>
+class NewArrival extends Component {
+
+    constructor(props) {
+        super(props);
+        this.next=this.next.bind(this);
+        this.previous=this.previous.bind(this)
+
+    }
+    next(){
+        this.slider.slickNext();
+    }
+    previous(){
+        this.slider.slickPrev();
+    }
+
+    render() {
+
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            autoplaySpeed:3000,
+            autoplay:true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: true,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+
+        return (
+            <Container className="text-center BetweenTwoSection" fluid={true}>
+                <h4 className="section-title px-0 mx-0 ">NEW ARRIVAL
+                    <a className="btn btn-sm ml-2 site-btn" onClick={this.previous} >
+                        <i className="fa fa-angle-left"></i>
+                    </a>
+                    <a className="btn btn-sm ml-2 site-btn" onClick={this.next}>
+                        <i className="fa fa-angle-right"></i>
+                    </a>
+                </h4>
+                <h6 className="section-sub-title pb-3">Some Of Our Exclusive Collection, You May Like</h6>
+                    <Slider  ref={c=>(this.slider=c)}   {...settings}>
+                        <div className="p-1">
+                            <Card className="card w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="p-1">
+                            <Card className="card w-100 image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="p-1">
+                            <Card className="card w-100   image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="p-1">
+                            <Card className="card w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="p-1">
+                            <Card className="card w-100  image-box ">
+                                <img src="https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg"/>
+                                <Card.Body>
+                                    <h5 className="product-name-on-card">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                    <p className="product-price-on-card">Price: 3000TK</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Slider>
+
+
+            </Container>
         );
     }
 }
- 
-export default Newarraival;
+
+export default NewArrival;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,10 +2,12 @@ import React, { Component, Fragment } from 'react';
 import Categories from '../components/home/Categories';
 import Collection from '../components/home/Collection';
 import FeaturedProducts from '../components/home/FeaturedProducts';
+import NewArrival from '../components/home/Newarraival';
 class HomePage extends  Component {
     render() { 
         return(
             <Fragment>
+                <NewArrival />
                 <FeaturedProducts />
                 <Categories />
                 <Collection />
